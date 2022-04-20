43e9c244f7f2d32c86d3ab039a5e3bfb5dce81b4
MargueriteLts <marguerite.lorthiois@outlook.fr>
2022-04-20 23:58:39 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//let firstName = 'Marg';
+// let lastName = 'Ls';
+// console.log(name);
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+//
+// const interestRateTwo = 0.3;
+// console.log(interestRateTwo);
+
+let name = 'Marg';
+let age = 222;
+let isApproved = false;
+let firstName = undefined;//puisqu'on a attribue la valeur undefined, la var est de type undefined
+let lastNanme = null;//type object
+
+let person = {
+  name: 'Marg',
+  age: 222
+};
+
+person.name = 'New';
+
+let selection = 'New';
+person[selection] = 'New';
+
+let selectedColors = ['red', 'green'];
+selectedColors[2] = 1;
+console.log(selectedColors)
+
+function greet(name, lastName) {
+  console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('People', 'of the world');
+greet('You', 'bad bitch');
